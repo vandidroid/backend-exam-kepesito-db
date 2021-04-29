8bc6d9fc709c93f960c5f01c75772af2111879ef
Andi <1vandidroid1@gmail.com>
2021-04-29 11:22:05 +0200

    Implement method

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +14,25 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+
+    public List<String> checkOverpopulation() {
+        List<String> dinos = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT breed FROM dinosaur WHERE actual - expected > 0 ORDER BY breed ASC";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                dinos.add(resultSet.getString("breed"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return dinos;
     }
 
 }
